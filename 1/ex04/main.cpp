7b500bea17527525966a6a0aa55f49ff30b1fb33
Jacquet BENJAMIN <jabenjam@e2r12p18.clusters.42paris.fr>
2022-01-28 12:12:15 +0100

    fixed files creating when not existing etc

--- 1/ex04/main.cpp
+++ 1/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 11:58:17 by jabenjam          #+#    #+#             */
-/*   Updated: 2022/01/25 14:48:09 by jabenjam         ###   ########.fr       */
+/*   Updated: 2022/01/28 12:10:42 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,40 @@ int		main(int ac, char **av)
 	if (ac != 4)
 		return (1);
 	std::string		replace = av[1];
-	replace.append(".replace");
+	replace += ".replace";
 	std::string		buffer;
 	std::string		s1 = av[2];
 	std::string		s2 = av[3];
-	std::ifstream	myFile(av[1]);
-	std::size_t		pos;
+	int				pos = 0;
 
-	// myFile.open(av[1], std::fstream::in | std::fstream::app);
+	if (ac != 4 && s1[0] == '\0')
+		return (1);
+	std::ifstream	myFile(av[1]);
 	if (myFile.is_open())
 	{
-		std::ofstream	replaceFile(replace);
-		replaceFile.open(replace, std::fstream::out | std::fstream::trunc);
-		std::getline(myFile, buffer);
-		std::cout << buffer << std::endl;
-		pos = buffer.find(s1, 0);
-		std::cout << pos << std::endl;
-		// while (std::getline(myFile, buffer))
-		// {
-		// 	std::cout << buffer << std::endl;
-		// 	if (pos == std::string::npos)
-		// 		std::cout << "did not find any match" << std::endl;
-		// 	else 
-		// 		std::cout << "found a match at position " << pos << std::endl;
-		// 	while ((pos = buffer.find(s1, 0)))
-		// 	{
-		// 		buffer.erase(pos, s1.length());
-		// 		buffer.insert(pos, s2);
-		// 	}
-		// 	replaceFile << buffer;
-		// 	buffer.clear();
-		// }
-		replaceFile.close();
+		std::ofstream	replaceFile(replace.c_str(), std::ofstream::out | std::ofstream::trunc);
+		if (replaceFile.is_open())
+		{
+			while (std::getline(myFile, buffer))
+			{
+				while ((pos = buffer.find(s1)) != std::string::npos)
+				{
+					buffer.erase(pos, s1.length());
+					buffer.insert(pos, s2);
+				}
+				replaceFile << buffer;
+				if (!myFile.eof())
+					replaceFile << std::endl;
+				buffer.clear();
+			}
+			replaceFile.close();
+		}
+		myFile.close();
 	}
 	else
-		std::cout << "unable to open file"<< std::endl;
-	myFile.close();
+	{
+		std::cout << "File does not exist or is corrupted"<< std::endl;
+		return (1);
+	}
+	return (0);
 }
