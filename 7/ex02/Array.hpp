f2745ea0005513a7f2c5c6c90bcaed89d2f0695a
Jacquet BENJAMIN <jabenjam@student.42.fr>
2022-02-10 16:01:37 +0100

    need to figure out how to implement assignment with = or [] operators overload

--- 7/ex02/Array.hpp
+++ 7/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 13:21:04 by jabenjam          #+#    #+#             */
-/*   Updated: 2022/02/10 14:28:46 by jabenjam         ###   ########.fr       */
+/*   Updated: 2022/02/10 16:00:46 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,56 +19,69 @@ template <typename T>
 class Array
 {
 	public:
+
+		// CONSTRUCTORS
 		Array() : _size(1) {this->_content = new T[1]();};
 		Array(unsigned int n) : _size(n) {this->_content = new T[n]();};
-		Array(Array const & src) : _size(src.getSize())
+		Array(Array const & src) : _size(src.size())
 		{
 			this->_content = new T[this->_size];
 			for (unsigned int i = 0; i < this->_size; i++)
 				this->_content[i] = src.getContent(i);
 		}
+
+		// DESTRUCTOR
 		~Array() {delete[] this->_content;};
 
-		unsigned int	getSize() const {return (this->_size);};
+		// GETTERS
+		unsigned int	size() const {return (this->_size);};
 		T				getContent(unsigned int i) const {return (i < this->_size ? this->_content[i] : 0);};
 
-
-		Array &			operator=(T const & rhs)
+		// OPERATOR OVERLOADS
+		Array &			operator=(Array const & rhs)
 		{
 			if (this == &rhs)
 				return (*this);
 			delete[] this->_content;
-			this->_size = rhs.getSize();
+			this->_size = rhs.size();
 			this->_content = new T[this->_size];
 			for (unsigned int i = 0; i < this->_size; i++)
 				this->_content[i] = rhs.getContent(i);
+			return (*this);
 		}
 		T				operator[](unsigned int i)
 		{
-			if (i < this->getSize())
+			if (i < this->size())
 				return (this->getContent(i));
 			else
-				throw std::exception();
+				throw Array::OutOfBounds();
 			return (0);
 		}
 		const T			operator[](unsigned int i) const
 		{
-			if (i < this->getSize())
+			if (i < this->size())
 				return (this->getContent(i));
 			else
-				throw std::exception();
+				throw Array::OutOfBounds();
 			return (0);
 		}
+		// EXCEPTIONS
+		class OutOfBounds : public std::exception
+		{
+			public:
+				virtual const char* what() const throw() {return ("Index is out of bounds");};
+		};
 	private:
 		T				*_content;
 		unsigned int	_size;
 };
 
+// STREAM INSERTION OPERATOR OVERLOAD
 template <typename T>
 std::ostream &			operator<<(std::ostream & o, Array<T> const & i)
 {
-	o << "Array contains " << i.getSize() << " elements :" << std::endl;
-	for (unsigned int idx = 0; idx < i.getSize(); idx++)
+	o << "Array contains " << i.size() << " elements :" << std::endl;
+	for (unsigned int idx = 0; idx < i.size(); idx++)
 		o << "[" << idx << "] = " << i.getContent(idx) << std::endl;
 	return (o);
 }
--- 7/ex02/main.cpp
+++ 7/ex02/main.cpp
@@ -6,30 +6,81 @@
 /*   By: jabenjam <jabenjam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 13:27:31 by jabenjam          #+#    #+#             */
-/*   Updated: 2022/02/10 14:25:24 by jabenjam         ###   ########.fr       */
+/*   Updated: 2022/02/10 15:34:49 by jabenjam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#define MAX_VAL 750
 
 int		main()
 {
-	Array<int>			ints(5);
-	Array<int>			ints2(ints);
-	Array<int>			ints3(2);
-	Array<float>		floats(5);
-	Array<double>		doubles(5);
-	Array<char>			chars(5);
-	Array<std::string>	strings(5);
+	{
+		// MY TESTS
+		Array<int>			ints(5);
+		Array<int>			ints2(ints);
+		Array<int>			ints3(2);
+		Array<float>		floats(5);
+		Array<double>		doubles(5);
+		Array<char>			chars(5);
+		Array<std::string>	strings(5);
 
-	std::cout << "INTS2 :" << std::endl << ints2 << std::endl;
-	std::cout << "INTS3 :" << std::endl << ints3 << std::endl;
-	ints3 = ints2;
-	std::cout << "INTS3 :" << std::endl << ints3 << std::endl;
-	std::cout << "INTS :" << std::endl << ints << std::endl;
-	std::cout << "FLOATS :" << std::endl << floats << std::endl;
-	std::cout << "DOUBLES :" << std::endl << doubles << std::endl;
-	std::cout << "CHARS :" << std::endl << chars << std::endl;
-	std::cout << "STRINGS :" << std::endl << strings;
-	return (0);
+		std::cout << "INTS :" << std::endl << ints << std::endl;
+		std::cout << "INTS2 :" << std::endl << ints2 << std::endl;
+		std::cout << "INTS3 BEFORE USING = OPERATOR :" << std::endl << ints3 << std::endl;
+		ints3 = ints2;
+		std::cout << "INTS3 AFTER USING = OPERATOR :" << std::endl << ints3 << std::endl;
+		std::cout << "FLOATS :" << std::endl << floats << std::endl;
+		std::cout << "DOUBLES :" << std::endl << doubles << std::endl;
+		std::cout << "CHARS :" << std::endl << chars << std::endl;
+		std::cout << "STRINGS :" << std::endl << strings;
+	}
+	{
+		// SUBJECT TESTS
+		Array<int> numbers(MAX_VAL);
+    	int* mirror = new int[MAX_VAL];
+    	srand(time(NULL));
+    	for (int i = 0; i < MAX_VAL; i++)
+    	{
+    	    const int value = rand();
+    	    numbers[i] = value;
+    	    mirror[i] = value;
+    	}
+    	//SCOPE
+    	{
+    	    Array<int> tmp = numbers;
+    	    Array<int> test(tmp);
+    	}
+	
+    	for (int i = 0; i < MAX_VAL; i++)
+    	{
+    	    if (mirror[i] != numbers[i])
+    	    {
+    	        std::cerr << "didn't save the same value!!" << std::endl;
+    	        return 1;
+    	    }
+    	}
+    	try
+    	{
+    	    numbers[-2] = 0;
+    	}
+    	catch(const std::exception& e)
+    	{
+    	    std::cerr << e.what() << '\n';
+    	}
+    	try
+    	{
+    	    numbers[MAX_VAL] = 0;
+    	}
+    	catch(const std::exception& e)
+    	{
+    	    std::cerr << e.what() << '\n';
+    	}
+    	for (int i = 0; i < MAX_VAL; i++)
+    	{
+    	    numbers[i] = rand();
+    	}
+    	delete [] mirror;
+    	return 0;
+	}
 }
